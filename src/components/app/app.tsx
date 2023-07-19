cba7950f7477fe6792e2fada068e3b8787cd39e8
uuliat1 <uatarik@mail.ru>
2023-07-19 10:35:34 +0300

    Adding routing

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,12 @@
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+import { AppRoute, AuthorizationStatus } from '../../const';
 import WelcomeScreen from '../../pages/main/main';
+import LoginScreen from '../../pages/login-page/login-page';
+import NotFound from '../../pages/not-found-page/not-found-page';
+import OfferScreen from '../../pages/offer-page/offer-page';
+import FavoritesScreen from '../../pages/favorites-page/favorites-page';
+import PrivateRoute from '../private-router/private-router';
 
 type AppProps = {
     placesCount: number;
@@ -6,7 +14,25 @@ type AppProps = {
 
 function App({placesCount}: AppProps): JSX.Element {
   return (
-    <WelcomeScreen placesCount ={placesCount} />
+    <HelmetProvider>
+      <BrowserRouter>
+      <Routes>
+      <Route path={AppRoute.Main} element={<WelcomeScreen placesCount={placesCount} />} />
+      </Routes>
+      <Routes>
+      <Route path={AppRoute.Login} element={<LoginScreen />} />
+      </Routes>
+      <Routes>
+      <Route path={AppRoute.Favotites} element={<PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}><FavoritesScreen /></PrivateRoute>} />
+      </Routes>
+      <Routes>
+          <Route path={AppRoute.Offer} element={<OfferScreen />} />
+        </Routes>
+        <Routes>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </HelmetProvider>
   );
 }
 
--- src/const.tsx
+++ src/const.tsx
@@ -1,3 +1,16 @@
 export const Setting = {
   PlacesCount: 312,
 };
+
+export enum AppRoute {
+  Main = '/',
+  Login = '/login',
+  Favotites = '/favorites',
+  Offer = '/offer/:id',
+}
+
+export enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unknown = 'UNKNOWN',
+}
