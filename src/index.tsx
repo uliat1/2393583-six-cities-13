ab4278e7255b1aed9f4913f71a86a973b9c73503
uuliat1 <uatarik@mail.ru>
2023-08-14 18:13:35 +0300

    обработка ошибок взаимодействия с сервером

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,9 @@ import { CITIES } from './const';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import ErrorMessage from './components/error-message/error-message';
+import { checkAuthAction } from './store/api-actions';
+
+store.dispatch(checkAuthAction());
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
