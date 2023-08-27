18105c2ee1ac7cbf9acea543a85a7484cc2ba6fb
uuliat1 <uatarik@mail.ru>
2023-08-28 00:57:48 +0300

    не работает логин и карточка

--- src/pages/not-found-page/not-found-page.tsx
+++ src/pages/not-found-page/not-found-page.tsx
@@ -1,13 +1,21 @@
 import {Link} from 'react-router-dom';
 
+
 function NotFound(): JSX.Element {
   return (
     <>
+      <div>
+        <img src="img/ico-no-results.svg" width={100} alt="pic" className="center"></img>
+      </div>
       <h1 style={{fontSize: '48px', textAlign: 'center', marginTop: '40vh'}}>404. Page not found</h1>
-      <Link to="/" style={{fontSize: '20px', textAlign: 'center', display: 'block', textDecoration: 'underline'}}>Ошибка 404. Вернуться на главную</Link>
+      <Link to="/" style={{fontSize: '20px',
+        textAlign: 'center',
+        display: 'block',
+        textDecoration: 'underline'}}
+      >Вернуться на главную
+      </Link>
     </>
   );
-
 }
 
 export default NotFound;
